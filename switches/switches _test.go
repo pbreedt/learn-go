8702ae159f46cdf2260e813e1cb3e646554dd16c
Petrus Breedt <petrus.breedt@adaptit.com>
2025-09-22 15:15:01 -0500

    add to switch samples

--- switches/switches _test.go
+++ switches/switches _test.go
@@ -5,7 +5,96 @@ import (
 )
 
 func TestWithExpression(t *testing.T) {
-	WithExpression()
-	WithExtendedExpression()
-	WithoutExpression()
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"something", "it's something"},
+		{"this", "it's this or that"},
+		{"that", "it's this or that"},
+		{"other", "it's probably nothing"},
+	}
+
+	for _, test := range tests {
+		result := WithExpression(test.input)
+		if result != test.expected {
+			t.Errorf("WithExpression(%q) = %q; want %q", test.input, result, test.expected)
+		}
+	}
+}
+
+func TestWithExtendedExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"so", "got short"},
+		{"somevalue", "got some"},
+		{"other", "got something else"},
+	}
+
+	for _, test := range tests {
+		result := WithExtendedExpression(test.input)
+		if result != test.expected {
+			t.Errorf("WithExtendedExpression(%q) = %q; want %q", test.input, result, test.expected)
+		}
+	}
+}
+
+func TestWithoutExpression(t *testing.T) {
+	tests := []struct {
+		input    int
+		expected string
+	}{
+		{5, "Mmm, small."},
+		{10, "Just right."},
+		{15, "Ohh, big!"},
+	}
+
+	for _, test := range tests {
+		result := WithoutExpression(test.input)
+		if result != test.expected {
+			t.Errorf("WithoutExpression(%d) = %q; want %q", test.input, result, test.expected)
+		}
+	}
+}
+
+func TestWithFallthroughMultipleCases(t *testing.T) {
+	tests := []struct {
+		input    int
+		expected string
+	}{
+		{2, "Less than five\nLess than eight\nLess than ten\n"},
+		{5, "Less than eight\nLess than ten\n"},
+		{6, "Less than eight\nLess than ten\n"},
+		{8, "Less than ten\n"},
+		{9, "Less than ten\n"},
+		{10, "Greater than or equal to ten\n"},
+		{11, "Greater than or equal to ten\n"},
+	}
+
+	for _, test := range tests {
+		result := WithFallthroughMultipleCases(test.input)
+		if result != test.expected {
+			t.Errorf("WithFallthroughMultipleCases(%d) = %q; want %q", test.input, result, test.expected)
+		}
+	}
+}
+
+func TestWithBreak(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"a b\nc", "a\nb\n"},
+		{"hello world", "h\ne\nl\nl\no\nw\no\nr\nl\nd\n"},
+		{"go\nlang", "g\no\n"},
+	}
+	for _, test := range tests {
+		result := WithBreak(test.input)
+		if result != test.expected {
+			t.Errorf("WithBreak(%s) = %q; want %q", test.input, result, test.expected)
+		}
+	}
+
 }
--- switches/switches.go
+++ switches/switches.go
@@ -2,34 +2,76 @@ package switches
 
 import "fmt"
 
-func WithExpression() {
-	value := "something"
-
+// ExactMatch example
+func WithExpression(value string) string {
 	switch value {
 	case "something":
-		fmt.Println("it's something")
+		return "it's something"
+	case "this", "that":
+		return "it's this or that"
 	default:
-		fmt.Println("it's probably nothing")
+		return "it's probably nothing"
 	}
 }
 
-func WithExtendedExpression() {
-	switch value := "something"; value {
-	case "something":
-		fmt.Println("it's something")
+// ExactMatch example
+func WithExtendedExpression(value string) string {
+	if len(value) < 3 {
+		return "got short"
+	}
+
+	switch start := value[0:4]; start {
+	case "some":
+		return "got some"
 	default:
-		fmt.Println("it's probably nothing")
+		return "got something else"
 	}
 }
 
-func WithoutExpression() {
-	i := 10
+// Range example
+func WithoutExpression(i int) string {
 	switch {
 	case i > 10:
-		fmt.Println("Ohh, big!")
+		return "Ohh, big!"
 	case i < 10:
-		fmt.Println("Mmm, small.")
+		return "Mmm, small."
 	default:
-		fmt.Println("Just right.")
+		return "Just right."
 	}
+}
+
+// Fallthrough example
+func WithFallthroughMultipleCases(i int) string {
+	result := ""
+	switch {
+	case i < 5:
+		result = "Less than five\n"
+		fallthrough
+	case i < 8:
+		result += "Less than eight\n"
+		fallthrough
+	case i < 10:
+		result += "Less than ten\n"
+	default:
+		result += "Greater than or equal to ten\n"
+	}
+	return result
+}
+
+func WithBreak(value string) string {
+	result := ""
+
+Loop:
+	for _, ch := range value {
+		switch ch {
+		case ' ': // skip/ignore space
+			break
+		case '\n': // break at newline
+			break Loop
+		default:
+			result += fmt.Sprintf("%c\n", ch)
+		}
+	}
+
+	return result
 }
